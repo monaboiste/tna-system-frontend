2e264ad2132229fbb4230dfa3539fecb6a86cfea
Monika Szumna <monikaszumna4@gmail.com>
2021-06-05 16:27:18 +0200

    Hided admin tab, when user with no admin rights is logged in

--- authcheck.js
+++ authcheck.js
@@ -1,18 +1,14 @@
-
-const userRole = localStorage.getItem('userRole');
-let adminOnlyElements = document.querySelectorAll('.admin');
-console.log(adminOnlyElements);
-if (userRole === 'ADMIN') {
-    adminOnlyElements.forEach(element => {
-        element.style.display = 'block';
-    })
-} else {
-    adminOnlyElements.forEach(element => {
-        element.style.display = 'none';
-    })
-}
-
 const loggedIn = localStorage.getItem('loggedIn');
 if (!loggedIn) {
     document.location.replace('login.html');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const userRole = localStorage.getItem('userRole');
+    let adminOnlyElements = document.querySelectorAll('.admin');
+    if (userRole === 'ADMIN') {
+        adminOnlyElements.forEach(element => {
+            element.style.display = 'block';
+        });
+    }
+});
